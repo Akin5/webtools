d03168696bd73817da2d4200cc4602084396b1bb
akin5 <akin@programmer.net>
2020-04-12 15:18:52 +0700

    update #20 Rapihin Dark mode

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,22 +1,22 @@
 $(window).on('load', function () {
 	setTimeout(() => {
 		$('body').addClass('loaded');
+		window.localStorage.getItem('dm')
+			? ($('#wrapper, .sidebar, .navbar, .footer').addClass('dark'),
+			  $('#darkmode').attr('checked', 'checked'),
+			  $('[name="theme-color"]').attr('content', '#213040'))
+			: ($('#wrapper, .sidebar, .navbar, .footer').removeClass('dark'),
+			  $('[name="theme-color"]').attr('content', '#4e73df'));
 	}, 1000);
 	$(document).ready(function () {
-		if (window.localStorage.getItem('dm')) {
-			$('#wrapper, .sidebar, .navbar, .footer').addClass('dark');
-			$('#darkmode').attr('checked', 'checked');
-		} else {
-			$('#wrapper, .sidebar, .navbar, .footer').removeClass('dark');
-		}
 		$('#darkmode').on('change', function () {
-			if (this.checked) {
-				window.localStorage.setItem('dm', true);
-				$('#wrapper, .sidebar, .navbar, .footer').addClass('dark');
-			} else {
-				window.localStorage.removeItem('dm');
-				$('#wrapper, .sidebar, .navbar, .footer').removeClass('dark');
-			}
+			this.checked
+				? (window.localStorage.setItem('dm', true),
+				  $('#wrapper, .sidebar, .navbar, .footer').addClass('dark'),
+				  $('[name="theme-color"]').attr('content', '#213040'))
+				: (window.localStorage.removeItem('dm'),
+				  $('#wrapper, .sidebar, .navbar, .footer').removeClass('dark'),
+				  $('[name="theme-color"]').attr('content', '#4e73df'));
 		});
 		$('#sidebarToggle, #sidebarToggleTop').on('click', function () {
 			$('body').toggleClass('sidebar-to');
